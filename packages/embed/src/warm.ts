e0abc35617ed4326487721b5106cafef0d7f6893
katipally <yashwanthreddy3471@gmail.com>
2026-07-05 16:04:10 -0700

    updated docs

--- packages/embed/src/warm.ts
+++
@@ -1,6 +0,0 @@
-import { warmEmbedder } from "./index.js";
-import { warmReranker } from "./rerank.js";
-
-console.log("Warming embedding + reranker models (one-time download)…");
-await Promise.all([warmEmbedder(), warmReranker()]);
-console.log("Models ready.");
